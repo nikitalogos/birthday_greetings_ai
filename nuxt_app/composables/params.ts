6b052bd638d2a89e1dd32a551a2ab05ce0fc7aa7
Nikita Logos <thelogosnik@gmail.com>
2023-07-16 16:40:19 +0300

    add params

---
+++ nuxt_app/composables/params.ts
@@ -0,0 +1,158 @@
+import { reactive, computed } from "vue";
+
+export const params = reactive({
+  groups() {
+    return [
+      {name: "essentials", label: "Essentials", items: [this.date_of_birth, this.name]},
+      {name: "dynamic", label: "Dynamic", items: [this.age, this.zodiac]},
+      {name: "person_details", label: "Person details", items: [this.gender, this.relationship, this.profession, this.hobby]},
+      {name: "content", label: "Content", items: [this.what_to_wish, this.target_language]},
+      {name: "comment", label: "Comment", items: [this.comment]},
+      {name: "styling", label: "Styling", items: [this.use_emojis, this.greeting_length, this.greeting_style, this.theme]},
+    ]
+  },
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~essentials~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  date_of_birth: {
+    label: "Date of Birth",
+    type: "date",
+    value: null,
+  },
+  name: {
+    label: "Name",
+    type: "text",
+    value: null,
+  },
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~dynamic~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  hide_age: false,
+  hide_zodiac: true,
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~person details~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  gender: {
+    label: "Gender",
+    type: "select",
+    value: null,
+    options: ["male", "female", "non-binary", "prefer not to say", "type your variant"],
+  },
+  relationship: {
+    label: "Relationship",
+    type: "select",
+    value: null,
+    options: [
+      "child", "parent", "partner", "spouse", "sibling", "grandparent", "cousin",
+      "friend", "colleague", "mentor", "boss", "acquaintance", "your variant"
+    ],
+  },
+  profession: {
+    label: "Profession",
+    type: "text",
+    value: null,
+  },
+  hobby: {
+    label: "Hobby",
+    type: "text",
+    value: null,
+  },
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~content~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  what_to_wish: {
+    label: "What to wish",
+    type: "multiselect",
+    value: null,
+    options: ["health", "wealth", "happiness", "success", "love", "adventure"],
+  },
+  target_language: {
+    label: "Target language",
+    type: "select",
+    value: null,
+    options: ["en", "ru"],
+  },
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~comment~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  comment: {
+    label: "Comment",
+    type: "textarea",
+    value: null,
+  },
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~styling~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  use_emojis: {
+    label: "Use emojis",
+    type: "select",
+    value: null,
+    options: ["no", "few", "more", "lots of"],
+  },
+  greeting_length: {
+    label: "Greeting length",
+    type: "select",
+    value: null,
+    options: ["short", "medium", "long", "your variant"],
+  },
+  greeting_style: {
+    label: "Greeting style",
+    type: "select",
+    value: null,
+    options: ["formal", "informal", "funny", "your variant"],
+  },
+  theme: {
+    label: "Theme",
+    type: "select",
+    value: null,
+    options: ["cyberpunk", "your variant"],  // todo fill themes
+  },
+
+  use_quotation: {
+    label: "Use quotation",
+    type: "checkbox",
+    value: null,
+  },
+//  use_affirmation: {
+//    label: "Use affirmation",
+//    type: "checkbox",
+//    value: null,
+//  },
+
+});
+
+params.age = computed(() => {
+  const now = new Date();
+  const date = params.date_of_birth;
+  if (!date) return null;
+
+  let was_birthday_this_year = now.getMonth() > date.getMonth() ||
+    (now.getMonth() === date.getMonth() && now.getDate() >= date.getDate());
+
+  return now.getFullYear() - date.getFullYear() - (wasBirthdayThisYear ? 0 : 1);
+});
+
+const ZODIAC_SIGNS = [
+    { sign: ["Capricorn", "♑"], date: [1, 19] },
+    { sign: ["Aquarius", "♒"], date: [2, 18] },
+    { sign: ["Pisces", "♓"], date: [3, 20] },
+    { sign: ["Aries", "♈"], date: [4, 19] },
+    { sign: ["Taurus", "♉"], date: [5, 20] },
+    { sign: ["Gemini", "♊"], date: [6, 20] },
+    { sign: ["Cancer", "♋"], date: [7, 22] },
+    { sign: ["Leo", "♌"], date: [8, 22] },
+    { sign: ["Virgo", "♍"], date: [9, 22] },
+    { sign: ["Libra", "♎"], date: [10, 22] },
+    { sign: ["Scorpio", "♏"], date: [11, 21] },
+    { sign: ["Sagittarius", "♐"], date: [12, 21] },
+    { sign: ["Capricorn", "♑"], date: [12, 31] } // to handle the case of dates after Dec 21
+];
+
+params._zodiac = computed(() => {
+  const date = params.date_of_birth;
+  if (!date) return null;
+
+  for(let zodiacSign of ZODIAC_SIGNS) {
+      if (date.getMonth() + 1 < zodiacSign.date[0] ||
+          (date.getMonth() + 1 === zodiacSign.date[0] && date.getDate() <= zodiacSign.date[1])) {
+          return zodiacSign;
+      }
+  }
+  throw new Error("Date is invalid. That's weird. Shouldn't get here");
+});
+params.zodiac_text = computed(() => params._zodiac.sign[0]);
+params.zodiac_emoji = computed(() => params._zodiac.sign[1]);
--- nuxt_app/pages/index.vue
+++ nuxt_app/pages/index.vue
@@ -1,7 +1,31 @@
 <script>
-export default defineNuxtComponent({});
+export default defineNuxtComponent({
+  data() {
+    return {
+      params,
+    }
+  },
+  created() {
+    // init params from query string
+    const date_str = this.$route.query.date_of_birth ?? "";
+    this.params.date_of_birth.value = isNaN(Date.parse(date_str)) ? null : new Date(date_str);
+    this.params.name.value = this.$route.query.name ?? null;
+  },
+});
 </script>
 
-<template>Hello world!</template>
+<template>
+  <div>
+    <form>
+      <div v-for="group, idx in params.groups()", :key="idx">
+        <h2>{{ group.label }}</h2>
+        <div v-for="param in group.items">
+          <label :for="param.name">{{ param.label }}</label>
+          <input type="param.type" :id="param.name" v-model="param.value" />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
 
 <style lang="scss"></style>
